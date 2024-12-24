0647585d1cd2f696cb8fb73e1e799a285d51a81b
RZN <nachiketa3299@gmail.com>
2024-12-24 23:19:55 +0900

    Sliding Box 소리 구현 완료

--- Assets/Scripts/Interactables/ActivationEvents/ActivateionEventSO_CardboardPile.cs
+++ Assets/Scripts/Interactables/ActivationEvents/ActivateionEventSO_CardboardPile.cs
@@ -1,4 +1,4 @@
-
+using FMODUnity;
 using UnityEngine;
 
 namespace BM.Interactables
@@ -6,6 +6,8 @@ namespace BM.Interactables
 	[CreateAssetMenu(menuName = "BM/SO/Activation Events/CardboardPile", fileName = "ActivationEventSO_CardboardPile")]
 	public class ActivationEventSO_CardboardPile : ActivationEventSO
 	{
+		[SerializeField] private EventReference m_movingSound;
+
 		public override void StartActivation(InteractAction _, InteractableBase sceneInteractable)
 		{
 			base.StartActivation(_, sceneInteractable);
@@ -15,6 +17,8 @@ namespace BM.Interactables
 			if (sceneInteractable.TryGetComponent<Animator>(out Animator animator))
 			{
 				animator.SetTrigger("Move");
+
+				RuntimeManager.PlayOneShot(m_movingSound);
 			}
 		}
 
